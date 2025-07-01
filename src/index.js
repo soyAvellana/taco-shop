8a6a63e181b05243e955493a39f405e94b68a7b1
Hazel <hazelhmrcu@live.de>
2025-07-01 21:21:47 +0200

    Feierabend

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
 import Navbar from './components/navbar/navbar.jsx';
+import Burritos from './components/menu/mains/burritos.jsx';  
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Navbar />
+    <Burritos />
     
   </React.StrictMode>
 );
